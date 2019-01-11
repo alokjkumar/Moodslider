b0a5e0467e75b42fbeaeb174d384c65ace45b926
Alok Kumar <me@alokjkumar.co.uk>
2019-01-11 20:30:57 +0000

    Slider Component Functionality

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'slider',
@@ -7,21 +7,38 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SliderComponent implements OnInit {
 
-  slideVal: number = 0;
-  @Input() minVal: string;
-  @Input() maxVal: string;
-  minLabel: string;
-  maxLabel: string;
+  slideVal: number = 0;                           // Value of the range input
+  output: string = "";                            // String output
+  @Input() minVal: string;                        // Input (minVal)
+  @Input() maxVal: string;                        // Input (maxVal)
+  @Input() id: string;                            // Input (ID)
+  minLabel: string;                               // String label (min)
+  maxLabel: string;                               // String label (max)
+  @Output() update = new EventEmitter<string>();   // Output event emitter
 
-  constructor() {
-
-  }
+  constructor() {}
 
   private createLabel(val: string): string {
+    // Convert the value to a label for display
     return val.toLowerCase().charAt(0).toUpperCase() + val.slice(1).toLowerCase().replace("_", " ");
   }
 
+  public getValue(value: number): string {
+    // Get the previous value so that we don't emit the same event twice
+    let prevVal = this.output;
+    // Convert the numerical value on the slider to string
+    if(value == 1) { this.output = this.maxVal; }
+    if(value == -1) { this.output = this.minVal; }
+    if(value == 0) { this.output = ""; }
+
+    if(prevVal != this.output) {
+      // Output the data to be picked up by the parent component
+      this.update.emit({data: this.output, id: this.id});
+    }
+  }
+
   ngOnInit() {
+    // Create the labels once the component is ready
     this.minLabel = this.createLabel(this.minVal);
     this.maxLabel = this.createLabel(this.maxVal);
   }
--- src/app/sliders/sliders.component.ts
+++ src/app/sliders/sliders.component.ts
@@ -7,16 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SlidersComponent implements OnInit {
 
-  sliderVals: any [] = [
-    {minVal: "agitated", maxVal: "calm"},
-    {minVal: "happy", maxVal: "sad"},
-    {minVal: "tired", maxVal: "wide_awake"},
-    {minVal: "scared", maxVal: "fearless"}
-  ]
+  // All the slider values (put this in an external file and import for more sliders)
+  sliderVals: any = [
+    {minVal: "agitated", maxVal: "calm", id: "0"},
+    {minVal: "happy", maxVal: "sad", id: "1"},
+    {minVal: "tired", maxVal: "wide_awake", id: "2"},
+    {minVal: "scared", maxVal: "fearless", id: "3"}
+  ];
 
-  constructor() { }
+  profile: any [] = [];
 
-  ngOnInit() {
+  constructor() {
+    for(let val of this.sliderVals) {
+      // We initialise our profile array so that we can update the values later
+      this.profile.push("");
+    }
+  }
+
+  ngOnInit() { }
+
+  addToProfile(data) {
+    // We update each array value individually. This allows us to overwrite values that are no longer relevant without logging previous values and crosschecking between various arrays
+    this.profile[parseInt(data.id)] = data.data;
   }
 
 }
