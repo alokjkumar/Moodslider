b3243753570ede15aac02a2d71ee1b7db86f8c31
Alok Kumar <me@alokjkumar.co.uk>
2019-01-11 23:02:47 +0000

    moodFilter Pipe

--- src/app/mood-filter.pipe.ts
+++ src/app/mood-filter.pipe.ts
@@ -1,12 +1,22 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'moodFilter'
+  name: 'moodFilter',
+  pure: false           // Pipe needs to be impure in order to detect changes in mood
 })
 export class MoodFilterPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(items: any[], moods: any[]): any[] {
+    // Check input arguments
+    if(!items) return [];
+    if(!moods) return [];
+
+    return items.filter((item) => {       // Apply a filter to the input items
+      return item.mood.some((r) => {      // Check item.mood array
+        return moods.indexOf(r) >= 0;     // Check if any of moods appears in item.mood (boolean)
+      });
+    });
+
   }
 
 }
--- src/app/programme/programme.component.ts
+++ src/app/programme/programme.component.ts
@@ -13,7 +13,6 @@ export class ProgrammeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
